9507f65e6645abbbe31d57f1f6b8179b13ac4e4b
rahul kumar <rahullobi00@gmail.com>
2021-12-01 14:34:05 +0545

    SetState

--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -6,21 +6,18 @@ class ClassComponent extends React.Component{
   constructor(){
     super();
     this.state={
-      'player':'ronaldo ',
-      
-      
-    }
-    this.state={'name':'Rahul'}
-    this.state={'country':'Japan'}
+      name:'rahul'
+  }
+}
+  chamename=()=>{
+    this.setState({name:'kumar'})
   }
   
   
     render(){
         return<div>
-        <h1>This is {this.state.player}</h1>
-        <h2>this is also {this.state.player}</h2>
         <h2>this is also {this.state.name}</h2>
-        <h1>this is also {this.state.country}</h1>
+        <button onClick={this.chamename}> change name </button>
         </div>
         
     }
