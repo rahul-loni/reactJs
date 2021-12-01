807939fa9aa05d7e4269cfbffd6e5cef92eec832
rahul kumar <rahullobi00@gmail.com>
2021-12-01 15:16:32 +0545

    Event Handeling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
 // import ClassComponent from './ClassComponent';
-import Parent from './Parent';
+// import Parent from './Parent';
 
 
 function App() {
+
+  function getalert(){
+    alert("The Button is Clicked")
+  }
+  function inputalert(){
+    alert("The input is Clicked")
+  }
   return (
     <div className="App">
-      <Parent movies='Borle Bhasiya pani me '/>
+      
+      {/* <Parent movies='Borle Bhasiya pani me '/> */}
       {/* <ClassComponent/> */}
+      <h1>Event Handeling</h1>
+      <button onClick={getalert}>Click Me</button><br/>
+      <input type="text" onChange={inputalert}/>
+      <h1 onMouseOver={getalert}>Hover Event </h1>
     </div>
   );
 }
